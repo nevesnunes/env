046f1564a2e85fe255a1125c470f4e1dec8ec125
nevesnunes <9061071+nevesnunes@users.noreply.github.com>
2021-06-07 22:50:12 +0100

    sync

--- linux/bin/xworkspace.sh
+++ linux/bin/xworkspace.sh
@@ -1,61 +1,66 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+set -eu
 
 # Check dependecies
 script_name=$(basename "$0")
 for i in xdotool wmctrl; do
   command -v "$i" > /dev/null 2>&1
-  if [[ $? -eq 1 ]]; then
+  if [ $? -eq 1 ]; then
     notify-send "[$script_name] Error" "$i not in \$PATH."
     exit 1
   fi
 done
 
 # Get the last occupied workspace
-number='^[0-9]+$'
+number='^[0-9]\+$'
 target=0
 workspaces=$(wmctrl -l | awk '{print $2}' | sort -u)
-while read -r workspace; do
-  if [[ $workspace =~ $number ]]; then
-    if [[ $workspace -gt $target ]]; then
+set -f; IFS='
+'
+for workspace in $workspaces; do
+  set +f; unset IFS
+  if echo "$workspace" | grep -q "$number"; then
+    echo "$workspace"
+    if [ "$workspace" -gt $target ]; then
       # Use intermediate workspace, if next one in list
       # doesn't match next expected number
-      if [[ $workspace -gt $(($target + 1)) ]]; then
+      if [ "$workspace" -gt $(($target + 1)) ]; then
         break
       else
         target=$workspace
       fi
     fi
   fi
-done <<< "$workspaces"
+done
+set +f; unset IFS
 
 # Use the closest next free workspace
 target=$(($target + 1))
 
 # If no free workspace was found, default to last one
-if [[ $target -eq $(xdotool get_num_desktops) ]]; then
+if [ $target -eq "$(xdotool get_num_desktops)" ]; then
   target=$(($target - 1))
 fi
 
-while [ "$1" != "" ]; do
+while [ $# -gt 0 ]; do
   case $1 in
   -t|--target)
     shift
-    if [[ $1 =~ $number ]]; then
+    if echo "$1" | grep -q "$number"; then
       target=$1
     else
       notify-send "[$script_name] Error" "Invalid argument."
       exit 1
     fi
     ;;
-  -c|--change)
+  -m|--move-window)
     window_id="$(xdotool getactivewindow)"
-
     wmctrl -i -r "$window_id" -t "$target"
     wmctrl -s "$target"
     ;;
-  -s|--send)
+  -s|--send-window)
     window_id="$(xdotool getactivewindow)"
-
     wmctrl -i -r "$window_id" -t "$target"
     ;;
   -g|--goto)
@@ -63,14 +68,34 @@ while [ "$1" != "" ]; do
     ;;
   -i|--previous)
     target=$(( $(xdotool get_desktop) - 1))
-    if [[ $target -ge 0 ]]; then
+    if [ $target -ge 0 ]; then
       wmctrl -s "$target"
     fi
     ;;
   -u|--next)
     target=$(( $(xdotool get_desktop) + 1))
-    if [[ $target -lt $(xdotool get_num_desktops) ]]; then
+    if [ $target -lt "$(xdotool get_num_desktops)" ]; then
       wmctrl -s "$target"
+    fi
+    ;;
+  -I|--move-window-to-previous)
+    target=$(( $(xdotool get_desktop) - 1))
+    if [ $target -ge 0 ]; then
+      window_id="$(xdotool getactivewindow)"
+      wmctrl -i -r "$window_id" -t "$target"
+      sleep 0.1
+      wmctrl -s "$target"
+      wmctrl -a "$window_id"
+    fi
+    ;;
+  -U|--move-window-to-next)
+    target=$(( $(xdotool get_desktop) + 1))
+    if [ $target -lt "$(xdotool get_num_desktops)" ]; then
+      window_id="$(xdotool getactivewindow)"
+      wmctrl -i -r "$window_id" -t "$target"
+      sleep 0.1
+      wmctrl -s "$target"
+      wmctrl -a "$window_id"
     fi
     ;;
   esac
