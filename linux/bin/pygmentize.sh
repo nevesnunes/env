d86073a9018fd2ff8b94d8c8b2ef7c1412a4af58
nevesnunes <9061071+nevesnunes@users.noreply.github.com>
2025-02-09 13:00:56 +0000

    sync

--- linux/bin/pygmentize.sh
+++ linux/bin/pygmentize.sh
@@ -2,10 +2,18 @@
 
 set -eu
 
-if ! command -v pygmentize >/dev/null 2>&1; then
+if ! command -v pygmentize > /dev/null 2>&1; then
   exec cat "$@"
-elif find ~/.local/lib/ -name pygments-paper.egg-link | grep -q .; then
-  exec pygmentize -f paper "$@"
 else
-  exec pygmentize -f terminal "$@"
+  if find ~/.local/lib/ -name pygments-paper.egg-link | grep -q .; then
+    formatter=paper
+  else
+    formatter=terminal
+  fi
+
+  if test -f "$1" && (echo "$1" | grep -q '\.bin$' || file --mime-type "$1" | grep -q 'application/octet-stream'); then
+    exec xxd -l 4096 "$1" | pygmentize -f "$formatter" -l hexdump
+  else
+    exec pygmentize -f "$formatter" "$@"
+  fi
 fi
